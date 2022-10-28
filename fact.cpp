dc70026a38e062b82a80cc782fa3393da2764536
its-me-01 <116364556+its-me-01@users.noreply.github.com>
2022-10-28 14:05:24 +0530

    improves readability and documentation

--- fact.cpp
+++ fact.cpp
@@ -1,18 +1,12 @@
 #include<iostream>
 using namespace std;
-
-
 int fact_recur(int n){
     if (n==1) return 1;
     return n * fact_recur(n-1);
 }
-
-
-
-int main(int argc, char const *argv[])
-{
-
+void main(){
     int n;
-    cin >> n; cout << fact_recur(n) << "\n";
-    return 0;
+    cout<<"Enter the number whose factorial has to find out :- "<<endl;
+    cin>>n;
+    cout<<"Factorial of "<<n<<" is "<<fact_recur(n)<<"\n";
 }
