8480bd9dbb574bea9b6a3489f17cd8922dedd318
its-me-01 <116364556+its-me-01@users.noreply.github.com>
2022-10-29 11:09:43 +0530

    increase readability and reduce code memory

--- vowel_or_not.cpp
+++ vowel_or_not.cpp
@@ -1,46 +1,49 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-
-int main(){
-
+void vowel(){                                                           //func if x is vowel
+    cout<<"Entered character "<<x<<" is a vowel."<<endl;
+}
+void not_vowel(){                                                       //func if x in not vowel
+    cout<<"Entered character "<<x<<" is not a vowel."<<endl;
+}
+void main(){
     char x;
-    cin >> x;
-    switch (x)
-    {
-    case 'A':
-        cout << "Vowel\n";
-        break;
-    case 'E':
-        cout << "Vowel\n";
-        break;
-    case 'I':
-        cout << "Vowel\n";
-        break;
-    case 'O':
-        cout << "Vowel\n";
-        break;
-    case 'U':
-        cout << "Vowel\n";
-        break;
-    case 'a':
-        cout << "Vowel\n";
-        break;
-    case 'e':
-        cout << "Vowel\n";
-        break;
-    case 'i':
-        cout << "Vowel\n";
-        break;
-    case 'o':
-        cout << "Vowel\n";
-        break;
-    case 'u':
-        cout << "Vowel\n";
-        break;
-    default:
-        cout << "Not a vowel\n";
-        break;
+    cout<<"Enter a character : "<<endl;
+    cin >> x;                                                           //taking input
+    switch (x){                                                         //using switch case
+        case 'A':
+            vowel();
+            break;
+        case 'E':
+            vowel();
+            break;
+        case 'I':
+            vowel();
+            break;
+        case 'O':
+            vowel();
+            break;
+        case 'U':
+            vowel();
+            break;
+        case 'a':
+            vowel();
+            break;
+        case 'e':
+            vowel();
+            break;
+        case 'i':
+            vowel();
+            break;
+        case 'o':
+            vowel();
+            break;
+        case 'u':
+            vowel();
+            break;
+        default:                                                            //if above conditions are not true
+            not_vowel();
+            break;
     }
-    return 0;
 }
